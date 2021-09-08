b571ec82fcf96d33275865f4f9922b2f12284601
jonathan serafini <jonathan.serafini@gmail.com>
2021-09-09 00:19:08 +0200

    add fetch

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,6 +1,7 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+var Highcharts = require('highcharts'); 
 
 const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
@@ -12,6 +13,8 @@ const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
   if (mapElement) { // only build a map if there's a div#map to inject into
+
+    const results = document.querySelector("#results");
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
@@ -31,8 +34,59 @@ const initMapbox = () => {
         mapboxgl: mapboxgl }));
     
     map.on('click', (e) => {
-        console.log(`A click event has occurred at ${e.lngLat}`);
+        console.dir(e.lngLat);
+
+        const planet_id = Math.ceil(Math.random() * 60);
+        fetch(`https://swapi.dev/api/planets/${planet_id}`)
+        .then(response => response.json())
+        .then((data) => {
+            results.innerHTML = `
+            <h3>Résulats</h3>
+            <p>Latitude: ${e.lngLat.lat}</p>
+            <p>Longitude: ${e.lngLat.lng}</p>
+            <p>Star Wars Planet Name: ${data["name"]}</p>
+            `
+            console.log(data);
+
+            Highcharts.chart('chart-container', {
+                chart: {
+                    type: 'line'
+                },
+                title: {
+                    text: 'Monthly Average Temperature'
+                },
+                subtitle: {
+                    text: 'Source: WorldClimate.com'
+                },
+                xAxis: {
+                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                },
+                yAxis: {
+                    title: {
+                        text: 'Temperature (°C)'
+                    }
+                },
+                plotOptions: {
+                    line: {
+                        dataLabels: {
+                            enabled: true
+                        },
+                        enableMouseTracking: false
+                    }
+                },
+                series: [{
+                    name: 'Tokyo',
+                    data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+                }, {
+                    name: 'London',
+                    data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+                }]
+            });
+          
+
+
         });
+    });
 
   }
 };
