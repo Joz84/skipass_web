306604cb800e22663ac61320f7bc18671e898b7b
jonathan serafini <jonathan.serafini@gmail.com>
2021-09-08 23:39:58 +0200

    add charts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,6 +21,7 @@ ActiveStorage.start()
 // External imports
 import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
+var Highcharts = require('highcharts'); 
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -29,4 +30,42 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
+
+  Highcharts.chart('chart-container', {
+      chart: {
+          type: 'line'
+      },
+      title: {
+          text: 'Monthly Average Temperature'
+      },
+      subtitle: {
+          text: 'Source: WorldClimate.com'
+      },
+      xAxis: {
+          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+      },
+      yAxis: {
+          title: {
+              text: 'Temperature (°C)'
+          }
+      },
+      plotOptions: {
+          line: {
+              dataLabels: {
+                  enabled: true
+              },
+              enableMouseTracking: false
+          }
+      },
+      series: [{
+          name: 'Tokyo',
+          data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+      }, {
+          name: 'London',
+          data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+      }]
+  });
+
+
+
 });
