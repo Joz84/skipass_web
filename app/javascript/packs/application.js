4f0937c03465dc5cf9cce5d731e043d12fd97a0a
jonathan serafini <jonathan.serafini@gmail.com>
2021-09-10 14:56:52 +0200

    add css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,5 +30,7 @@ document.addEventListener('turbolinks:load', () => {
   // initSelect2();
   initMapbox();
 
+  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
 
 });
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -48,54 +48,57 @@ const initMapbox = () => {
         const numStation = bestStation(e.lngLat.lat, e.lngLat.lng, markers)
         console.dir(numStation)
 
-        fetch(`${apiUrl}/?id_station=${numStation}`)
+        fetch(`${apiUrl}?id_station=${numStation}`)
+        // fetch(`https://swapi.dev/api/people/1/`)
         .then(response => response.json())
         .then((data) => {
+            const response = data
+            // const response = {'t': 25.5, 'p': 1000, 'u': 70, 'r': 1}
             results.innerHTML = `
-            <h3>Résulats</h3>
+            <h3>Prédiction Machine Learning</h3>
             <p>Latitude: ${e.lngLat.lat}</p>
             <p>Longitude: ${e.lngLat.lng}</p>
-            <p>temperature dans 6h: ${data['t']}</p>
-            <p>pression dans 6h: ${data['p']}</p>
-            <p>humidité dans 6h: ${data['u']}</p>
-            <p>pluie dans 6h: ${data['r'] ? 'oui' : 'non'}</p>
+            <p>temperature dans 6h: ${response['t']}</p>
+            <p>pression dans 6h: ${response['p']}</p>
+            <p>humidité dans 6h: ${response['u']}</p>
+            <p>pluie dans 6h: ${response['r'] ? 'oui' : 'non'}</p>
             `
-            console.log(data);
+            console.log(response);
 
-            Highcharts.chart('chart-container', {
-                chart: {
-                    type: 'line'
-                },
-                title: {
-                    text: 'Monthly Average Temperature'
-                },
-                subtitle: {
-                    text: 'Source: WorldClimate.com'
-                },
-                xAxis: {
-                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-                },
-                yAxis: {
-                    title: {
-                        text: 'Temperature (°C)'
-                    }
-                },
-                plotOptions: {
-                    line: {
-                        dataLabels: {
-                            enabled: true
-                        },
-                        enableMouseTracking: false
-                    }
-                },
-                series: [{
-                    name: 'Tokyo',
-                    data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-                }, {
-                    name: 'London',
-                    data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-                }]
-            });
+            // Highcharts.chart('chart-container', {
+            //     chart: {
+            //         type: 'line'
+            //     },
+            //     title: {
+            //         text: 'Monthly Average Temperature'
+            //     },
+            //     subtitle: {
+            //         text: 'Source: WorldClimate.com'
+            //     },
+            //     xAxis: {
+            //         categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            //     },
+            //     yAxis: {
+            //         title: {
+            //             text: 'Temperature (°C)'
+            //         }
+            //     },
+            //     plotOptions: {
+            //         line: {
+            //             dataLabels: {
+            //                 enabled: true
+            //             },
+            //             enableMouseTracking: false
+            //         }
+            //     },
+            //     series: [{
+            //         name: 'Tokyo',
+            //         data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+            //     }, {
+            //         name: 'London',
+            //         data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+            //     }]
+            // });
           
         });
     });
